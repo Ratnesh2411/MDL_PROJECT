27d0e7149babaf7653c55bacd78bf97fe3b1ab56
Dev Shinde <84491890+dev-shinde@users.noreply.github.com>
2024-11-12 20:18:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def predict():
 
     return render_template('index.html', prediction=class_label, confidence=confidence)
 
-if os.name == 'main':
+if __name__ == '__main__': 
     # Use the PORT environment variable or default to 8000
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
